479c414b2d64d74211070eff6583687fe8f7e892
Mark Erik <markerik2016@gmail.com>
2025-05-24 02:34:21 +0300

    Added files

---
+++ index.js
@@ -0,0 +1,64 @@
+const card1 = document.getElementById('card-1')
+const card2 = document.getElementById('card-2')
+const card3 = document.getElementById('card-3')
+
+let currentIndex = 0;
+
+document.addEventListener('click', (e) => {
+    if (currentIndex === 3) {
+        currentIndex = 0
+    }
+    else if (e.target.id === 'next-btn') {
+        if (currentIndex === 0) {
+            card1.style.left = '-71%'
+            card2.style.left = '10%'
+            card3.style.left = '91%'
+            card1.classList.remove('active')
+            card2.classList.add('active')
+            currentIndex++
+        } else if (currentIndex === 1) {
+            card2.style.left = '-71%'
+            card3.style.left = '10%'
+            card1.style.left = '91%'
+            card2.classList.remove('active')
+            card3.classList.add('active')
+            currentIndex++
+        } else if (currentIndex === 2) {
+            card3.style.left = '-71%'
+            card1.style.left = '10%'
+            card2.style.left = '91%'
+            card3.classList.remove('active')
+            card1.classList.add('active')
+            currentIndex++
+        }
+    }
+    else if (e.target.id === 'prev-btn') {
+        if (currentIndex === -1) {
+            currentIndex = 2
+        } else if (currentIndex === 0) {
+            card1.style.left = '91%'
+            card3.style.left = '10%'
+            card2.style.left = '-71%'
+            card1.classList.remove('active')
+            card3.classList.add('active')
+            currentIndex--
+        }
+        else if (currentIndex === 1) {
+            card2.style.left = '91%'
+            card1.style.left = '10%'
+            card3.style.left = '-71%'
+            card2.classList.remove('active')
+            card1.classList.add('active')
+            currentIndex--
+            
+        }
+        else if (currentIndex === 2) {
+            card3.style.left = '91%'
+            card2.style.left = '10%'
+            card1.style.left = '-71%'
+            card3.classList.remove('active')
+            card2.classList.add('active')
+            currentIndex--
+        }
+    }
+})
